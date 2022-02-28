9685df243fa7c2fd08dd35779db982dc328045d8
Aslam <iupin5212@gmail.com>
2022-02-28 21:55:53 +0700

    Account statement and matchTX

--- src/Traits/MutasibankAPI.php
+++ src/Traits/MutasibankAPI.php
@@ -4,41 +4,92 @@ namespace Indofx\Mutasibank\Traits;
 
 trait MutasibankAPI
 {
+    /**
+     * getCurrentUser
+     *
+     * @return \Aslam\Response\Response
+     */
     public function getCurrentUser()
     {
         return $this->send('GET', 'user');
     }
 
+    /**
+     * getAllAccount
+     *
+     * @return \Aslam\Response\Response
+     */
     public function getAllAccount()
     {
         return $this->send('GET', 'accounts');
     }
 
+    /**
+     * getAccountById
+     *
+     * @param  mixed $id
+     * @return \Aslam\Response\Response
+     */
     public function getAccountById($id)
     {
         return $this->send('GET', 'account/' . $id);
     }
 
+    /**
+     * validateTransaction
+     *
+     * @param  mixed $id
+     * @return \Aslam\Response\Response
+     */
     public function validateTransaction($id)
     {
         return $this->send('GET', 'validate/' . $id);
     }
 
+    /**
+     * rerunCheckMutasi
+     *
+     * @param  mixed $accountId
+     * @return \Aslam\Response\Response
+     */
     public function rerunCheckMutasi($accountId)
     {
         return $this->send('GET', 'rerun/' . $accountId);
     }
 
-    public function matchTransaction()
+    /**
+     * getAccountStatement
+     *
+     * @param  mixed $accountId
+     * @return \Aslam\Response\Response
+     */
+    public function getAccountStatement(int $accountId, string $dateFrom = '', string $dateTo = '')
     {
-        return $this->send('POST', 'match');
+        $options = ['form_params' => ['date_from' => $dateFrom, 'date_to' => $dateTo]];
+
+        if (!$dateFrom || !$dateTo) {
+            $options = [];
+        }
+
+        return $this->send('POST', 'statements/' . $accountId, $options);
     }
 
-    public function getAccountStatement($accountId)
+    /**
+     * matchTransaction
+     *
+     * @return \Aslam\Response\Response
+     */
+    public function matchTransaction($accountId, $amount)
     {
-        return $this->send('POST', 'statements/' . $accountId);
+        return $this->send('POST', 'match/' . $accountId, ['form_params' => ['amount' => $amount]]);
     }
 
+    /**
+     * onOffAccount
+     *
+     * @param  mixed $accountId
+     * @return \Aslam\Response\Response
+     */
     public function onOffAccount($accountId)
     {
         return $this->send('POST', 'on_off/' . $accountId);
--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'url' => env('MUTASIBANK_URL', 'https://mutasibank.co.id/api/v1'),
-    'token' => 'SDI2VVJ4a2JuT2dWMXl3cjdSZnQwTDlsanBkenptNUlUZFF5bmphVE1pVkx1SU9OazEzYk1CRnZadzBS621b45862e287',
+    'url' => env('MUTASIBANK_URL', 'https://mutasibank.co.id/api/v1/'),
+    'token' => env('MUTASIBANK_TOKEN', ''),
 ];
